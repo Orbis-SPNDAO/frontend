cc6322299cbae8453357c2ae0ed2310c88757da1
Tobias Leinss <caruso33@web.de>
2022-12-11 13:02:35 +0100

    Start dashboard overview

--- web/pages/dashboard.tsx
+++ web/pages/dashboard.tsx
@@ -1,16 +1,24 @@
-import { useRouter } from "next/router";
-import { useAccount } from "wagmi";
-import PageLayout from "../components/layouts/PageLayout";
-import UserDashData from "../components/UserDashData";
+import { ethers } from "ethers"
+import { useRouter } from "next/router"
+import { useAccount } from "wagmi"
+import PageLayout from "../components/layouts/PageLayout"
+import UserDashData from "../components/UserDashData"
 
 export default function Home() {
-  const router = useRouter();
-  const { isConnecting, address } = useAccount();
+  const router = useRouter()
+  const { isConnecting, address } = useAccount()
+
+  const overviewData = {
+    totalRewards: ethers.utils.parseEther("1.27"),
+    decryptionSessions: 5,
+    votesParticipated: 12,
+    discussions: 63,
+  }
 
   return (
     <>
       {!address || isConnecting ? (
-        <PageLayout containerClassName="w-full bg-cover min-h-screen">
+        <PageLayout containerClassName="bg-custom-blue bg-cover min-h-screen">
           <div className="text-center mt-20 min-w-full">
             <h1 className="font-bold text-custom-purple text-3xl leading-tight">
               Please Sign In To View Dashboard
@@ -18,23 +26,57 @@ export default function Home() {
           </div>
         </PageLayout>
       ) : (
-        <PageLayout containerClassName="w-full min-h-screen bg-cover">
-          <div className="text-center w-full mt-20">
-            <h1 className="font-bold text-custom-purple text-6xl leading-tight">
-              Dashboard
-            </h1>
-            <button
-              className="block mx-auto bg-custom-purple text-white text-bold text-xl rounded-xl mt-5 px-16 py-2"
-              onClick={() => router.push("/join")}
-            >
-              Mint
-            </button>
-            <div className="text-center mt-20">
-              <UserDashData />
+        <PageLayout containerClassName="bg-custom-blue bg-cover min-h-screen">
+          <div className="text-center my-5 md:my-10 w-full">
+            <div className="w-stretch m-5 md:mx-28 md:my-12 h-fit py-6 px-4 md:p-10 hero border-2">
+              <h1 className="text-xl md:text-4xl font-normal">
+                Your data is more valuable than you think
+              </h1>
+              <p className="text-sm mt-3">
+                Control your data and engage in the SPN DAO governance
+              </p>
+
+              <div className="m-5 md:my-12 h-fit py-6 px-4 flex justify-around border-2 rounded-lg font-normal">
+                <div className="flex flex-col">
+                  <div>
+                    <span className="text-4xl">
+                      {overviewData?.totalRewards
+                        ? ethers.utils.formatEther(overviewData.totalRewards)
+                        : "-"}
+                    </span>{" "}
+                    Matic
+                  </div>
+                  <div>Rewards</div>
+                </div>
+                <div className="flex flex-col">
+                  <div className="text-4xl">
+                    {overviewData?.decryptionSessions ?? "-"}
+                  </div>
+                  <div>Decryption Sessions</div>
+                </div>
+
+                <span className="h-10 w-0.5 my-auto bg-custom-border opacity-25"></span>
+
+                <div className="flex flex-col">
+                  <div className="text-4xl">
+                    {overviewData?.votesParticipated ?? "-"}
+                  </div>
+                  <div>Votes Participated</div>
+                </div>
+
+                <div className="flex flex-col">
+                  <div className="text-4xl">
+                    {overviewData?.discussions ?? "-"}
+                  </div>
+                  <div>Discussions</div>
+                </div>
+              </div>
             </div>
+
+            <div>X</div>
           </div>
         </PageLayout>
       )}
     </>
-  );
+  )
 }
