d9f5d7729eca931b22af89522d1cb9297379fef3
Brian Grosso <bgro63@gmail.com>
2022-12-14 00:02:16 -0500

    default export

--- web/pages/come-back.tsx
+++ web/pages/come-back.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 
-export const ComeBack: FC = () => {
-    return <div>Come Back</div>
-}
+const ComeBack: FC = () => {
+  return <div>Come Back</div>;
+};
+
+export default ComeBack;
