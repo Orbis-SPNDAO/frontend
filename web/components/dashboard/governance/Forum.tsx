3ad4617dbff59959a48e5e9fead607d013c513b7
Jonathan Conn <jonathansconn@gmail.com>
2022-12-20 11:37:52 -0500

    loading user posts in

--- web/components/dashboard/governance/Forum.tsx
+++ web/components/dashboard/governance/Forum.tsx
@@ -1,10 +1,39 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Orbis } from "@orbisclub/orbis-sdk";
 
+let orbis = new Orbis();
+
+interface IPosts {
+  data: [],
+  error: string,
+  status: number
+}
+  
+
+
 export default function Forum() {
-  let orbis = new Orbis();
 
   const [user, setUser] = useState();
+  const [posts, setPosts] = useState({} as IPosts);
+  
+  useEffect(() => {
+    const getPosts = async () => {
+      let res = await orbis.isConnected();
+      if (res.status == 200) {
+        let posts = await orbis.getPosts({
+          did: res.did,
+        });
+        return posts;
+      } else {
+        console.log("need to connect to orbis");
+        return [];
+      }
+    };
+    
+    getPosts().then((posts) => {
+      setPosts(posts);
+    });
+  }, []);
 
   const newPost = async (e: any) => {
     e.preventDefault();
@@ -15,14 +44,14 @@ export default function Forum() {
       let description = e.target.description.value;
 
       let post = await orbis.createPost({
-        "title": title, 
-        "body": description
+        title: title,
+        body: description,
       });
       console.log(post);
     } else {
-      await connect();
+      console.log("need to connect to orbis");
     }
-  }
+  };
 
   async function connect() {
     let res = await orbis.connect();
@@ -46,10 +75,31 @@ export default function Forum() {
 
       <div className="p-5">
         <form onSubmit={newPost}>
-        <input type={"text"} placeholder={"Title"} name="title" />
-        <input type={"text"} placeholder={"Description"} name="description" />
-        <button>Submit</button>
+          <input type={"text"} placeholder={"Title"} name="title" />
+          <input type={"text"} placeholder={"Description"} name="description" />
+          <button>Submit</button>
         </form>
+      </div>
+
+      <div className="p-5">
+        <h1>My Posts</h1>
+        <>
+        {
+          !posts || !posts.data ? (
+            <p>No posts found</p>
+          ) : (
+            posts.data.map((post: any) => {
+              console.log(post)
+              return (                
+                <div key={post.timestamp} className="p-5">
+                  <h1>{post.content.title}</h1>
+                  <h2>{post.content.body}</h2>                
+                </div>
+              );
+            })
+          )                    
+        }
+        </>
       </div>
     </div>
   );
