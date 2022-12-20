9f64f18535fb84d48aa6f086841a617edac685cd
Jonathan Conn <jonathansconn@gmail.com>
2022-12-20 11:11:56 -0500

    create post working

--- web/components/dashboard/governance/Forum.tsx
+++ web/components/dashboard/governance/Forum.tsx
@@ -1,17 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Orbis } from "@orbisclub/orbis-sdk";
 
 export default function Forum() {
   let orbis = new Orbis();
 
   const [user, setUser] = useState();
-  
+
+  const newPost = async (e: any) => {
+    e.preventDefault();
+    // console.log(e.target.title.value);
+    let res = await orbis.isConnected();
+    if (res.status == 200) {
+      let title = e.target.title.value;
+      let description = e.target.description.value;
+
+      let post = await orbis.createPost({
+        "title": title, 
+        "body": description
+      });
+      console.log(post);
+    } else {
+      await connect();
+    }
+  }
+
   async function connect() {
     let res = await orbis.connect();
-    
+
     if (res.status == 200) {
       setUser(res.did);
-      console.log(res)
+      console.log(res);
     } else {
       console.log("Error connecting to Ceramic: ", res);
       alert("Error connecting to Ceramic.");
@@ -20,11 +38,20 @@ export default function Forum() {
 
   return (
     <div>
-			{user ?
-				<p>Connected with: {user}</p>
-			:
-				<button onClick={() => connect()}>Connect</button>
-			}
-		</div>
+      {user ? (
+        <p>Connected with: {user}</p>
+      ) : (
+        <button onClick={() => connect()}>Connect</button>
+      )}
+
+      <div className="p-5">
+        <form onSubmit={newPost}>
+        <input type={"text"} placeholder={"Title"} name="title" />
+        <input type={"text"} placeholder={"Description"} name="description" />
+        <button>Submit</button>
+        </form>
+      </div>
+    </div>
   );
 }
+//
