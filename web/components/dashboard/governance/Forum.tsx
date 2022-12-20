43b087fc7afdf56d287e9d35cd026c5a52995a3f
Jonathan Conn <jonathansconn@gmail.com>
2022-12-20 10:41:20 -0500

    added back in custom types folder, orbis connection working in forms

--- web/components/dashboard/governance/Forum.tsx
+++ web/components/dashboard/governance/Forum.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Orbis } from "@orbisclub/orbis-sdk";
 
 export default function Forum() {
+  let orbis = new Orbis();
+
+  const [user, setUser] = useState();
+  
+  async function connect() {
+    let res = await orbis.connect();
+    
+    if (res.status == 200) {
+      setUser(res.did);
+      console.log(res)
+    } else {
+      console.log("Error connecting to Ceramic: ", res);
+      alert("Error connecting to Ceramic.");
+    }
+  }
+
   return (
-    <div>Forum</div>
-  )
+    <div>
+			{user ?
+				<p>Connected with: {user}</p>
+			:
+				<button onClick={() => connect()}>Connect</button>
+			}
+		</div>
+  );
 }
