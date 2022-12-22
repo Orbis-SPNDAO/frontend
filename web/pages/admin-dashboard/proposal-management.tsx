e2ada35a9a3c6d9c7c5e18864fdd0180aea43af1
Jonathan Conn <jonathansconn@gmail.com>
2022-12-22 12:38:19 -0500

    added contract call to get all sbt holders to pass to vocdoni proposal creation

--- web/pages/admin-dashboard/proposal-management.tsx
+++ web/pages/admin-dashboard/proposal-management.tsx
@@ -1,51 +1,49 @@
 import { VocdoniSDKClient, EnvOptions, AccountData } from "@vocdoni/sdk";
 import { FC, useEffect, useState, useRef } from "react";
-
-import { useSigner } from "wagmi";
+import { useSigner, useContractRead } from "wagmi";
+import { SBT_ABI } from "../../abis/currentABI";
 import PageLayout from "../../components/layouts/PageLayout";
 
-
 const ProposalManagement: FC = () => {
-  
-  const client = useRef<VocdoniSDKClient>();  
+  const client = useRef<VocdoniSDKClient>();
   const vocAccount = useRef<AccountData>();
 
   const { data: signer } = useSigner();
+  
+  const { data: sbtHolders } = useContractRead({      
+    address: process.env.NEXT_PUBLIC_SBT_ADDR,
+    abi: SBT_ABI,
+    functionName: "fetchNfts",
+  });  
 
-  useEffect(() => {            
+  useEffect(() => {
     if (!client.current && signer)
-      client.current = new VocdoniSDKClient({env: EnvOptions.DEV, wallet: signer})
-
+      client.current = new VocdoniSDKClient({
+        env: EnvOptions.DEV,
+        wallet: signer,
+      });
   }, [signer]);
 
   useEffect(() => {
-        
     const accountHandler = async () => {
       try {
-        vocAccount.current = await client.current!.fetchAccountInfo();        
-      } catch (e) { // account not created yet
-        vocAccount.current = await client.current!.createAccount();        
-      }      
-      
-      // top up account with faucet tokens
-      if (vocAccount.current.balance === 0) {
-        await client.current!.collectFaucetTokens()
+        // account already exists
+        vocAccount.current = await client.current!.fetchAccountInfo();
+      } catch (e) {
+        // account not created yet
+        vocAccount.current = await client.current!.createAccount();
       }
 
-    }
+      // top up account with faucet tokens
+      if (vocAccount.current.balance === 0) {
+        await client.current!.collectFaucetTokens();
+      }
+    };
 
-    if (client.current)
-      accountHandler();      
-
+    if (client.current) accountHandler();
   }, [client]);
 
-  
-  
-  const createProposal = async () => {
-            
-  
-
-  };
+  const createProposal = async () => {};
 
   return (
     <PageLayout
