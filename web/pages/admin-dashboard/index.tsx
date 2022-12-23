4d55f44f812a0e22b21e1429149c791efbea31c5
Brian Grosso <bgro63@gmail.com>
2022-12-23 14:25:35 -0500

    version down wagmi and merge main in

--- web/pages/admin-dashboard/index.tsx
+++ web/pages/admin-dashboard/index.tsx
@@ -5,10 +5,8 @@ import { useEffect } from "react";
 import { ADMIN_ABI } from "../../abis/currentABI";
 
 import { useAccount, useContract, useProvider } from "wagmi";
-import AdminDashData from "../../components/AdminDashData";
-import Header from "../../components/Header";
-import Overview from "../../components/dashboards-shared/Overview";
 import { overviewData } from "../../components/dashboard/dummydata";
+import Overview from "../../components/dashboards-shared/Overview";
 import { SocialsFooter } from "../../components/SocialsFooter";
 
 export default function AdminDashboard() {
@@ -52,7 +50,7 @@ export default function AdminDashboard() {
         <Overview
           overviewData={overviewData}
           onClick1={() => router.push("/admin-dashboard/data-management")}
-          onClick2={() => null}
+          onClick2={() => router.push("/admin-dashboard/proposal-management")}
           isAdmin
         ></Overview>
         <SocialsFooter addClasses="absolute bottom-0 left-0 right-0 pb-4 sm:pb-8" />
