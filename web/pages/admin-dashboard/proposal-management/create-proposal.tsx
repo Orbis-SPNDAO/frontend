8f15fd7cbf4a98c47f9c0e13db5e28b892303d91
Tobias Leinss <tobias.leinss@mway.io>
2022-12-25 23:27:49 +0100

    Make options work

--- web/pages/admin-dashboard/proposal-management/create-proposal.tsx
+++ web/pages/admin-dashboard/proposal-management/create-proposal.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import Button from "../../../components/Button";
+import { AiFillCloseCircle } from "react-icons/ai";
+import Button, { ButtonStyle } from "../../../components/Button";
 import BackButton from "../../../components/dashboards-shared/BackButton";
 import PageLayout from "../../../components/layouts/PageLayout";
 
@@ -17,13 +18,60 @@ export default function CreateProposal() {
   const [description, setDescription] = useState("");
   const [discussion, setDiscussion] = useState("");
 
+  const [timeWindow, setTimeWindow] = useState("");
+  const [voteOptions, setVoteOptions] = useState<[string, string][]>([]);
+
   const isValidStep1 = title && description;
+  const isValidStep2 = timeWindow && voteOptions?.length > 1;
 
   function onStep1Continue() {
     if (!isValidStep1) return;
 
     console.log("onStep1Continue");
     setPageState(CreateProposalState.Step2VoteOptions);
+  }
+
+  function onStep2Continue() {
+    if (!isValidStep1) return;
+
+    console.log("onStep2Continue");
+    setPageState(CreateProposalState.Step2VoteOptions);
+  }
+
+  function onSelectTimeWindow() {
+    console.log("onSelectTimeWindow");
+  }
+
+  function onAddOption() {
+    setVoteOptions([...voteOptions, [(voteOptions.length + 1).toString(), ""]]);
+  }
+
+  function onRemoveOption(index: number) {
+    const options = [
+      ...voteOptions.slice(0, index),
+      ...(voteOptions.length - 1 >= index
+        ? voteOptions.slice(index + 1, voteOptions.length)
+        : []),
+    ];
+
+    const fixedIndexOpts = options.map((opt, index) => {
+      return [(index + 1).toString(), opt[1]] as [string, string];
+    });
+
+    setVoteOptions(fixedIndexOpts);
+  }
+
+  function updateVoteOptionText(text: string, index: number) {
+    const voteOption = voteOptions[index];
+    voteOption[1] = text;
+
+    setVoteOptions([
+      ...voteOptions.slice(0, index),
+      voteOption,
+      ...(voteOptions.length - 1 >= index
+        ? voteOptions.slice(index + 1, voteOptions.length)
+        : []),
+    ]);
   }
 
   return (
@@ -79,6 +127,72 @@ export default function CreateProposal() {
               <div className="col-span-1 ml-8">
                 <Button onClick={onStep1Continue} disabled={!isValidStep1}>
                   Continue
+                </Button>
+              </div>
+            </div>
+          )}
+
+          {pageState === CreateProposalState.Step2VoteOptions && (
+            <div className="grid grid-cols-3 mt-8 mb-16">
+              <div className="col-span-2 mr-8">
+                <div className="flex flex-col items-start">
+                  <span className="text-2xl mb-2">{title}</span>
+
+                  <span className="bg-custom-purple text-white rounded-full text-sm px-2 py-1">
+                    Overview
+                  </span>
+
+                  <span className="text-custom-gray text-sm my-8">
+                    {description}
+                  </span>
+                </div>
+
+                <div className="flex flex-col items-start">
+                  Cast your vote
+                  {voteOptions?.map((opt: [string, string], index: number) => {
+                    return (
+                      <>
+                        <div className="w-full relative" key={opt[0]}>
+                          <button
+                            className="opacity-100 hover:opacity-100 absolute text-custom-gray -right-2 top-0"
+                            onClick={() => onRemoveOption(index)}
+                          >
+                            <AiFillCloseCircle size={20} />
+                          </button>
+                        </div>
+
+                        <input
+                          type="text"
+                          className="mt-2 w-full text-center py-2 border-2 border-gray px-4 rounded-lg"
+                          placeholder={"Option" + opt[0]}
+                          value={opt[1]}
+                          onChange={(e) =>
+                            updateVoteOptionText(e.target.value, index)
+                          }
+                        />
+                      </>
+                    );
+                  })}
+                  <button
+                    className="w-full text-custom-purple mt-4"
+                    onClick={onAddOption}
+                  >
+                    Add another option
+                  </button>
+                </div>
+              </div>
+
+              <div className="col-span-1 ml-8">
+                <Button
+                  addClassName="mb-2"
+                  onClick={onSelectTimeWindow}
+                  buttonStyle={ButtonStyle.Outline}
+                >
+                  Select time window
+                </Button>
+
+                <Button onClick={onStep2Continue} disabled={!isValidStep2}>
+                  Publish
                 </Button>
               </div>
             </div>
