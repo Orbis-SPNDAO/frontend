030357eb149b018f59041fd6df0ccfc3f771d593
Jonathan Conn <jonathansconn@gmail.com>
2022-12-29 13:18:23 -0500

    moved vocdoni to create-proposal

--- web/pages/admin-dashboard/proposal-management.tsx
+++ web/pages/admin-dashboard/proposal-management.tsx
@@ -1,8 +1,5 @@
-import { AccountData, EnvOptions, VocdoniSDKClient } from "@vocdoni/sdk";
 import { useRouter } from "next/router";
 import { FC, useEffect, useRef } from "react";
-import { useContractRead, useSigner } from "wagmi";
-import { SBT_ABI } from "../../abis/currentABI";
 import Button from "../../components/Button";
 import {
   ProposalData,
@@ -15,44 +12,6 @@ import PageLayout from "../../components/layouts/PageLayout";
 
 const ProposalManagement: FC = () => {
   const router = useRouter();
-
-  const client = useRef<VocdoniSDKClient>();
-  const vocAccount = useRef<AccountData>();
-
-  const { data: signer } = useSigner();
-
-  const { data: sbtHolders } = useContractRead({
-    address: process.env.NEXT_PUBLIC_SBT_ADDR,
-    abi: SBT_ABI,
-    functionName: "fetchHolders",
-  });
-
-  useEffect(() => {
-    if (!client.current && signer)
-      client.current = new VocdoniSDKClient({
-        env: EnvOptions.DEV,
-        wallet: signer,
-      });
-  }, [signer]);
-
-  useEffect(() => {
-    const accountHandler = async () => {
-      try {
-        // account already exists
-        vocAccount.current = await client.current!.fetchAccountInfo();
-      } catch (e) {
-        // account not created yet
-        vocAccount.current = await client.current!.createAccount();
-      }
-
-      // top up account with faucet tokens
-      if (vocAccount.current.balance === 0) {
-        await client.current!.collectFaucetTokens();
-      }
-    };
-
-    if (client.current) accountHandler();
-  }, [client]);
 
   const createProposal = async () => {
     router.push("/admin-dashboard/proposal-management/create-proposal");
