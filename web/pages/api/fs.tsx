554e44f3afac2e9cecf80eb9083f93281659b8d8
Brian Grosso <bgro63@gmail.com>
2022-12-30 16:30:19 -0500

    upload partial fix

--- web/pages/api/fs.tsx
+++ web/pages/api/fs.tsx
@@ -32,7 +32,7 @@ export default async function handler(
       return res.status(200).json({ success: "true" });
     } catch (err) {
       console.log({ err });
-      return res.status(400).json({ success: "false" });
+      return res.status(400).json({ err });
     }
   } else if (req.method == "GET") {
     let { path } = req.query;
