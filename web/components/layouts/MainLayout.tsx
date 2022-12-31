c5365c66b6ef075feb1fa44ddc66850884931514
Jonathan Conn <jonathansconn@gmail.com>
2022-12-31 14:51:02 -0500

    changed site description to value prop

--- web/components/layouts/MainLayout.tsx
+++ web/components/layouts/MainLayout.tsx
@@ -12,7 +12,7 @@ const MainLayout: FC<MainLayoutProps> = ({ children }) => {
     <div>
       <Head>
         <title>SPN DAO</title>
-        <meta name="description" content="SPN DAO - Protect your data and profit!" />
+        <meta name="description" content="SPN DAO - Have true ownership of your data" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <link rel="icon" href="/favicon.png" />
       </Head>
